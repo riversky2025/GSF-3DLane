30413f0607c9c7f62c1891260a0ba5d7b4f757cd
riversky <riversky@126.com>
2026-06-20 16:52:22 +0800

    Update openlane_1000_baseline.py

--- config/gsf/openlane_1000_baseline.py
+++ config/gsf/openlane_1000_baseline.py
@@ -6,15 +6,16 @@ _base_ = [
     '../_base_/optimizer.py'
 ]
 
-mod = 'release_iccv/latr_1000_baseline_rio'
+mod = 'release_tip/latr_1000_baseline'
 mean = [0.485, 0.456, 0.406]
 std = [0.229, 0.224, 0.225]
 
 dataset = '1000'
-dataset_dir = '/data02/OpenLane/images/'
-data_dir = '/data02/OpenLane/lane3d_1000/'
+dataset_dir = '/dataset/openlane/openlane1.2/images/'
+data_dir = '/dataset/openlane/openlane1.2/lane3d_1000/'
 
-batch_size = 6
+
+batch_size = 8
 nworkers = 10
 num_category = 21
 pos_threshold = 0.3
@@ -22,8 +23,8 @@ pos_threshold = 0.3
 clip_grad_norm = 20
 
 top_view_region = np.array([
-    [-10, 103], [10, 103], [-10, 3], [10, 3]])
-enlarge_length = 20
+    [-10, 104.4], [10, 104.4], [-10, 2], [10, 2]])
+enlarge_length = 15.6
 position_range = [
     top_view_region[0][0] - enlarge_length,
     top_view_region[2][1] - enlarge_length,
@@ -31,7 +32,7 @@ position_range = [
     top_view_region[1][0] + enlarge_length,
     top_view_region[0][1] + enlarge_length,
     5.]
-anchor_y_steps = np.linspace(3, 103, 20)
+anchor_y_steps = np.linspace(2, 103, 20)
 num_y_steps = len(anchor_y_steps)
 
 # extra aug
@@ -41,8 +42,8 @@ photo_aug = dict(
     saturation_range=(0.5, 1.5),
     hue_delta=9)
 
-_dim_ = 256
-num_query = 40
+_dim_ = 184 
+num_query = 40  
 num_pt_per_line = 20
 em_bed_type='dc'
 dcg_cfg = dict(
@@ -54,47 +55,21 @@ dcg_cfg = dict(
         type='ResNet',
         depth=34,
         num_stages=4,
-        out_indices=(1, 2, 3),
+        out_indices=(3,),
         frozen_stages=-1,
-        norm_cfg=dict(type='BN2d', requires_grad=False),
-        norm_eval=True,
-        style='caffe',
-        init_cfg=dict(type='Pretrained', checkpoint='torchvision://resnet34'),
-        dcn=dict(type='DCNv2', deform_groups=1, fallback_on_stride=False),
-        stage_with_dcn=(False, False, False, False),
-    ),
-    neck=dict(
-        type='FPN',
-       in_channels=[128, 256, 512],
-         # in_channels=[512, 768, 1024],
-        # in_channels=[512, 1024, 2048],
-        out_channels=_dim_,
-        start_level=0,
-        add_extra_convs='on_output',
-        num_outs=4,
-        relu_before_extra_convs=True
+        norm_cfg=dict(type='BN2d', requires_grad=True),
+        norm_eval=False,
+        style='pytorch',
+        init_cfg=dict(type='Pretrained', checkpoint='torchvision://resnet34')
     ),
     pts_backbone=dict(
         type='SECOND',
-        in_channels=3,
+        in_channels=32,
         out_channels=[128, 256, 512],
-        layer_nums=[5, 5, 5],
+        layer_nums=[4, 2, 1],
         layer_strides=[8, 2, 1],
         norm_cfg=dict(type='BN', eps=0.001, momentum=0.01),
         conv_cfg=dict(type='Conv2d', bias=False)),
-    pts_neck=dict(
-        type='SECONDFPN',
-        in_channels=[128, 256, 512],
-        out_channels=[_dim_, _dim_, _dim_],
-        upsample_strides=[1, 2, 2],
-        norm_cfg=dict(type='BN', eps=0.001, momentum=0.01),
-        upsample_cfg=dict(type='deconv', bias=False),
-        use_conv_for_no_stride=True),
-
-    ms2one=dict(
-        type='DilateNaive',
-        inc=_dim_, outc=_dim_, num_scales=3,
-        dilations=(1, 2, 5)),
     head=dict(
         xs_loss_weight=2.0,
         zs_loss_weight=10.0,
@@ -173,7 +148,7 @@ transformer=dict(
         type='DCGTransformerDecoderV5',
         d3type=em_bed_type,
         embed_dims=_dim_,
-        num_layers=4,
+        num_layers=2,
         enlarge_length=enlarge_length,
         M_decay_ratio=1,
         num_query=num_query,
@@ -199,7 +174,7 @@ transformer=dict(
                     d3type=em_bed_type,
                     num_heads=4,
                     num_levels=1,
-                    num_points=8,
+                    num_points=4,  # Reduced from 8 to 4 to save FLOPs
                     batch_first=False,
                     num_query=num_query,
                     num_anchor_per_query=num_pt_per_line,
@@ -209,12 +184,12 @@ transformer=dict(
             ffn_cfgs=dict(
                 type='FFN',
                 embed_dims=_dim_,
-                feedforward_channels=_dim_*8,
+                feedforward_channels=_dim_*4,  # Reduced from _dim_*8 to _dim_*4
                 num_fcs=2,
                 ffn_drop=0.1,
                 act_cfg=dict(type='ReLU', inplace=True),
             ),
-            feedforward_channels=_dim_ * 8,
+            feedforward_channels=_dim_ * 4,  # Reduced from _dim_*8 to _dim_*4
             operation_order=('self_attn', 'norm', 'cross_attn', 'norm',
                             'ffn', 'norm')),
 ))
@@ -224,13 +199,22 @@ transformer=dict(
 resize_h = 720
 resize_w = 960
 
-nepochs = 300
-eval_freq = 1
+nepochs = 48
+eval_freq = 2
 optimizer_cfg = dict(
-    type='AdamW',
-    lr=2e-5,
+    type='AdamW', 
+    lr=2e-4,
+    betas=(0.95, 0.99),
     paramwise_cfg=dict(
         custom_keys={
-            'sampling_offsets': dict(lr_mult=0.1),
+            'sampling_offsets': dict(lr_mult=0.1, decay_mult=1.0),
         }),
     weight_decay=0.01)
+lr_config = dict(
+    policy='CosineAnnealing',
+    warmup='linear',
+    warmup_iters=500,
+    warmup_ratio=1.0 / 3,
+    min_lr=1e-6
+)
+lr_policy='CosineAnnealing'
